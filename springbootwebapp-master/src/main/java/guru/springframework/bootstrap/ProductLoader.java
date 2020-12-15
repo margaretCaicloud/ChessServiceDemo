b22afeca677b51f33a76467a8c87bd119fa695b4
margaretCaicloud <maoyeqin@caicloud.io>
2020-12-15 09:51:03 +0800

    web bug fix

--- springbootwebapp-master/src/main/java/guru/springframework/bootstrap/ProductLoader.java
+++ springbootwebapp-master/src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -27,19 +27,22 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
+        if(event.getApplicationContext().getDisplayName().equals("SpringClientFactory-provider")){
+            return;
+        }
         Product shirt = new Product();
-        shirt.setDescription("Spring Framework Guru Shirt");
+        shirt.setDescription("serviceA grpc");
         shirt.setPrice(new BigDecimal("18.95"));
         shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
-        shirt.setProductId("235268845711068308");
+        shirt.setProductId("1");
         productRepository.save(shirt);
 
         log.info("Saved Shirt - id: " + shirt.getId());
 
         Product mug = new Product();
-        mug.setDescription("Spring Framework Guru Mug");
+        mug.setDescription("serviceA db");
         mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
-        mug.setProductId("168639393495335947");
+        mug.setProductId("2");
         mug.setPrice(new BigDecimal("11.95"));
         productRepository.save(mug);
 
--- springbootwebapp-master/src/main/java/guru/springframework/controllers/ProductController.java
+++ springbootwebapp-master/src/main/java/guru/springframework/controllers/ProductController.java
@@ -3,6 +3,8 @@ package guru.springframework.controllers;
 import guru.springframework.domain.Product;
 import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class ProductController {
 
     private ProductService productService;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient lbClient;
+
+    private String ErukaServiceProvider = "provider";
+    private String rest_url_prefix = "http://"+ErukaServiceProvider+"/";
 
     @Autowired
     public void setProductService(ProductService productService) {
@@ -55,4 +66,34 @@ public class ProductController {
         return "redirect:/product/" + product.getId();
     }
 
+
+    @RequestMapping("product/{id}")
+    public String showProduct(@PathVariable Integer id, Model model){
+        Product tmp=productService.getProductById(id);
+        ServiceInstance si=lbClient.choose(ErukaServiceProvider);
+        if(null==si){
+            System.out.println("get service provider name fail! provider name:"+ErukaServiceProvider);
+            return null;
+        }
+        String url="http://"+si.getServiceId()+":"+si.getPort();
+
+        switch (id)
+        {
+            case 1:
+                url=url+"/grpc/grpcDate";
+                break;
+            default:
+                url=url+"/db/mary";
+        }
+
+        ResponseEntity<String> rep=restTemplate.exchange(url,
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<String>() {}
+        );
+
+        System.out.println("get service provider rep:"+rep.getBody());
+        tmp.setDescription(rep.getBody());
+        model.addAttribute("product", tmp);
+        return "productshow";
+    }
 }
